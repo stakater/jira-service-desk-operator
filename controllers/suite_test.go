f974955a2a34b93a078fc886259e2f5a048e4c2f
Rehan Javed <rehanjaved237@gmail.com>
2021-01-06 10:53:08 +0500

    Remove remnent resources after tests (#57)

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -140,7 +140,9 @@ var _ = BeforeSuite(func(done Done) {
 }, 60)
 
 var _ = AfterSuite(func() {
-	util.TryDeleteProject(mockData.CustomerTestProjectInput.Spec.Name, ns)
+	// Cleanup - Delete all remnent resources
+	util.DeleteAllProjects(ns)
+	util.DeleteAllCustomers(ns)
 
 	By("tearing down the test environment")
 	err := testEnv.Stop()
