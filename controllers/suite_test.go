9cb87dba28a0dbb81ba495d620f7a36c6a0ba7ff
stakater-user <stakater@gmail.com>
2021-05-06 06:15:41 +0000

    [skip-ci] Update artifacts

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -73,7 +73,7 @@ var _ = BeforeSuite(func(done Done) {
 
 	logger := zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter))
 	logf.SetLogger(logger)
-	
+
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
